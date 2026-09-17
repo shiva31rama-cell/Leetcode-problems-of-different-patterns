83a408c486ddc01ec741417b0b3e316c4d1a224f
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 01:06:04 +0530

    Add Java course schedule solution

---
+++ problems/19-topological-sort/001-course-schedule/java/Solution.java
@@ -0,0 +1,44 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0];
+            int before = prerequisite[1];
+            graph.get(before).add(course);
+            indegree[course]++;
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int processed = 0;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            processed++;
+
+            for (int next : graph.get(current)) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        return processed == numCourses;
+    }
+}
