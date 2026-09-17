72ab2a74e27d02bc54ce0c6bdd0296fbd154f62a
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 00:40:04 +0530

    feat: add Python greedy solution

---
+++ problems/12-greedy/001-jump-game/python/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        farthest = 0
+
+        for i, jump in enumerate(nums):
+            if i > farthest:
+                return False
+
+            farthest = max(farthest, i + jump)
+
+            if farthest >= len(nums) - 1:
+                return True
+
+        return True
