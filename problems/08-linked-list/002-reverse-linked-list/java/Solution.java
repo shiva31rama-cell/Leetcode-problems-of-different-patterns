03c7583be5833a60038f3752cca9673debc8b911
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 01:00:53 +0530

    Add Java solution for reverse linked list

---
+++ problems/08-linked-list/002-reverse-linked-list/java/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
