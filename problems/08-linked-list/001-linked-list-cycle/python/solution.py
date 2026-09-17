f376bf3ac85a262c6212c6f0de56f488fd4e50ac
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 00:39:08 +0530

    feat: add Python linked list cycle solution

---
+++ problems/08-linked-list/001-linked-list-cycle/python/solution.py
@@ -0,0 +1,19 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def hasCycle(self, head: ListNode | None) -> bool:
+        slow = head
+        fast = head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow is fast:
+                return True
+
+        return False
