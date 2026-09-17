bbbed4fab274a24b81790636ebc8c0a9b4e37a6a
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 01:11:48 +0530

    Add Java Floyd Warshall solution

---
+++ problems/30-floyd-warshall/001-all-pairs-shortest-path/java/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    private static final int INF = 1_000_000_000;
+
+    public int[][] floydWarshall(int[][] graph) {
+        int n = graph.length;
+        int[][] dist = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) dist[i][j] = graph[i][j];
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                if (dist[i][k] == INF) continue;
+                for (int j = 0; j < n; j++) {
+                    if (dist[k][j] == INF) continue;
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        return dist;
+    }
+}
