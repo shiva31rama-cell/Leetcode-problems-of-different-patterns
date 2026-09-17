43315d44c99b7088079b7a31274b1a45c32db7c8
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 01:00:12 +0530

    Add Python solution for 3Sum

---
+++ problems/02-two-pointers/002-3sum/python/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        answer = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    answer.append([nums[i], nums[left], nums[right]])
+
+                    left_value = nums[left]
+                    right_value = nums[right]
+
+                    while left < right and nums[left] == left_value:
+                        left += 1
+                    while left < right and nums[right] == right_value:
+                        right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return answer
