bf8d6e4571535005897c2eb75033785e48e6251e
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 01:00:08 +0530

    Add Java solution for 3Sum

---
+++ problems/02-two-pointers/002-3sum/java/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                long sum = (long) nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == rightValue) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
