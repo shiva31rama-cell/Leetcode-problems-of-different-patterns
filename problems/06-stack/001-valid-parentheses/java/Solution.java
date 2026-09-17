6c1338c17e609ae31407cdaf5b568ee2a2ee2bc9
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 00:38:33 +0530

    feat: add Java stack solution

---
+++ problems/06-stack/001-valid-parentheses/java/Solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char open = stack.pop();
+                if (!matches(open, ch)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean matches(char open, char close) {
+        return (open == '(' && close == ')')
+                || (open == '[' && close == ']')
+                || (open == '{' && close == '}');
+    }
+}
