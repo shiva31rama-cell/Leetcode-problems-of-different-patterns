76c5a694d68beef898825585b0b74ab2ed382b17
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 01:36:08 +0530

    Add Python distinct subsequences

---
+++ problems/35-string-dp/004-distinct-subsequences/python/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def num_distinct(self,s,t):
+        dp=[0]*(len(t)+1); dp[0]=1
+        for ch in s:
+            for j in range(len(t)-1,-1,-1):
+                if ch==t[j]: dp[j+1]+=dp[j]
+        return dp[-1]
