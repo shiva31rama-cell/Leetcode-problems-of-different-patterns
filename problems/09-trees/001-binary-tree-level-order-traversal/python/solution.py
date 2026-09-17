88b908f9039d904ecc74a81e8a5eb4948bc571e1
shiva31rama-cell <shiva31rama@gmail.com>
2026-09-18 00:39:26 +0530

    feat: add Python tree BFS solution

---
+++ problems/09-trees/001-binary-tree-level-order-traversal/python/solution.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode | None) -> list[list[int]]:
+        if root is None:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level = []
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+
+        return result
